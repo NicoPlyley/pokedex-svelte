aad1cda7f65f6f98e30a92d4729fa649d9351033
Nico Plyley <nplyley@gmail.com>
2022-07-25 18:33:37 -0500

    Added Pokemon store to console

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,10 @@
 <script>
-
+  import { pokemon } from '../stores/pokestore';
+  console.log($pokemon)
 </script>
 
 <svelte:head>
-    <title>Pokedex - Svelte Kit</title>
+  <title>Pokedex - Svelte Kit</title>
 </svelte:head>
 
 <h1>Svelte Kit Pokedex</h1>

7ef0d44bcbd2c119364669e329012fa568d81205
Nico Plyley <nplyley@gmail.com>
2022-07-25 19:26:11 -0500

    Added tailwind styles

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,2 +1,2 @@
-<h1>About</h1>
+<h1 class="text-4xl text-center my-8">About</h1>
 <p>A Svelte kit demo that uses the Pokemon API to build a Pokedex</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,17 @@
 <script>
   import { pokemon } from '../stores/pokestore';
-  console.log($pokemon)
+  import PokemanCard from '../components/pokemanCard.svelte'
 </script>
 
 <svelte:head>
   <title>Pokedex - Svelte Kit</title>
 </svelte:head>
 
-<h1>Svelte Kit Pokedex</h1>
+<h1 class="text-4xl text-center my-8">Svelte Kit Pokedex</h1>
+
+
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+  {#each $pokemon as pokeman}
+    <PokemanCard pokeman={pokeman} />
+  {/each}
+</div>
